8df39b390213dce13e55f5275567b73523b5e7b2
Deise Araújo <127044115+DehAraujo@users.noreply.github.com>
2026-03-12 06:25:18 +0000

    Atualiza exercicio 4

--- Exercicio04/src/cobertura_condicao.py
+++ Exercicio04/src/cobertura_condicao.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-
 def acesso(idade, membro):
     if idade >= 18 and membro:
         return "Permitido"
     return "Negado"
-=======
-def acesso(idade, membro):
-    if idade >= 18 and membro:
-        return "Permitido"
-    return "Negado"
->>>>>>> 6ac98827f8ff4e5a658841ee91c0b00ba7d214e6
--- Exercicio04/tests/test_classificar.py
+++ Exercicio04/tests/test_classificar.py
@@ -10,11 +10,7 @@ from src.classificar import classificar
 def test_classificar_caminhos_independentes(valor, valor_esperado):
     assert classificar(valor) == valor_esperado
 
-<<<<<<< HEAD
-#São necessários 2 testes para cobrir C1 já que a função só possui dois ramos (confuso dúvida)
-=======
 #São necessários 3 caminhos de testes para cobrir todos os ramos de C1
 #CT1 (x > 100) cobre o primeiro ramo verdadeiro da condição e encerra a execução com retorno "Alto".
 #CT2 (x <= 100 e x > 50) cobre o segundo ramo falso da condicao x > 100 e o segundo ramo da condicção verdadeira x > 50 e encerra a execução com retorno "Medio"
 #CT3 (x <= 50) cobre o ramo falso da primeira e segunda condição, alcançando o final de retorno "Baixo"
->>>>>>> 6ac98827f8ff4e5a658841ee91c0b00ba7d214e6
